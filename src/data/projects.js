d5be399a4731dfb6138c35d8588d0e878356cfb4
IMjaeyongpark <jaeyong36@naver.com>
2026-09-02 22:14:50 +0900

    feat: 나의 방송파트너 프로젝트 이미지 추가

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,7 @@
 import gitopsArchitecture from '../assets/projects/gitops-architecture.png'
 import gitopsPipeline from '../assets/projects/gitops-pipeline.png'
+import streamingPartnerArchitecture from '../assets/projects/streaming-partner-architecture.png'
+import streamingPartnerAwards from '../assets/projects/streaming-partner-awards.png'
 
 const getPeriodDates = (period) => {
   const dates = period.match(/\d{4}\.\d{2}/g) ?? []
@@ -204,9 +206,15 @@ const projectItems = [
       youtubeId: 'g4iZemAs8WM',
       title: '라이브 채팅 감정 분석 기술 프로젝트 소개 영상',
     },
-    architecture: '',
-    architectureAlt: '',
-    images: [],
+    architecture: streamingPartnerArchitecture,
+    architectureAlt: 'AWS 기반 나의 방송파트너 서비스 아키텍처',
+    images: [
+      {
+        src: streamingPartnerAwards,
+        alt: '나의 방송파트너 프로젝트 수상 내역',
+        caption: '교내외 공모전 수상 내역',
+      },
+    ],
   },
 ]
 
